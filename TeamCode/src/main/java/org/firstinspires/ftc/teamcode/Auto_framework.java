17204834922ff2ed0189af340bf301c71233548b
ccmsRobotics <ccmsRobotics@gccms.org>
2023-10-17 19:48:46 -0400

    apriltag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_framework.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_framework.java
@@ -124,7 +124,57 @@ public class Auto_framework extends LinearOpMode
             myPosition  = pipeline.getAnalysis();
             telemetry.addData("Analysis", myPosition);
             telemetry.update();
+            sleep(1000);
+            webcam.setPipeline(aprilTagDetectionPipeline);
+            ArrayList<AprilTagDetection> detections = aprilTagDetectionPipeline.getDetectionsUpdate();
+            if(detections != null)
+            {
+                telemetry.addData("FPS", webcam.getFps());
+                telemetry.addData("Overhead ms", webcam.getOverheadTimeMs());
+                telemetry.addData("Pipeline ms", webcam.getPipelineTimeMs());
+
+                // If we don't see any tags
+                if(detections.size() == 0)
+                {
+                    numFramesWithoutDetection++;
+
+                    // If we haven't seen a tag for a few frames, lower the decimation
+                    // so we can hopefully pick one up if we're e.g. far back
+                    if(numFramesWithoutDetection >= THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION)
+                    {
+                        aprilTagDetectionPipeline.setDecimation(DECIMATION_LOW);
+                    }
+                }
+                // We do see tags!
+                else
+                {
+                    numFramesWithoutDetection = 0;
+
+                    // If the target is within 1 meter, turn on high decimation to
+                    // increase the frame rate
+                    if(detections.get(0).pose.z < THRESHOLD_HIGH_DECIMATION_RANGE_METERS)
+                    {
+                        aprilTagDetectionPipeline.setDecimation(DECIMATION_HIGH);
+                    }
+
+                    for(AprilTagDetection detection : detections)
+                    {
+                        Orientation rot = Orientation.getOrientation(detection.pose.R, AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES);
+
+                        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+                        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
+                        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
+                        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
+                        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", rot.firstAngle));
+                        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", rot.secondAngle));
+                        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", rot.thirdAngle));
+                    }
+                }
+
+                telemetry.update();
+            }
             //Drive to location
+
 
 
             //Drop Pixel
