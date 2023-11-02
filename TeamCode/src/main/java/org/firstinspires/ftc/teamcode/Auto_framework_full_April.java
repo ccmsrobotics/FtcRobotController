954eb07a1bac8943ae32d9ec1416c80eb51d7985
ccmsRobotics <ccmsRobotics@gccms.org>
2023-11-02 17:01:07 -0400

    Adding EasyOpenCV Apriltag examples

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_framework_full_April.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_framework_full_April.java
@@ -22,6 +22,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -53,6 +54,7 @@ import java.util.ArrayList;
  * FTC Team 18975 autonomous code
  */
 @Autonomous (name="Red-Human-Auto April Tag navigation", group="Red")
+@Disabled
 public class Auto_framework_full_April extends LinearOpMode {
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
