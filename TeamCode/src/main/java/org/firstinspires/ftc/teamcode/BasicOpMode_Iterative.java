3acbb2a76c257c54dcaea636203f6657465533a8
bbrug <bbruggema73@gmail.com>
2024-10-01 21:24:57 -0400

    found the bug and renamed the TeleOp name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Iterative.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Iterative.java
@@ -52,7 +52,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@TeleOp(name="i wuv cheese", group="Iterative OpMode")
+@TeleOp(name="I wuv cheez(mechanum drive)", group="Iterative OpMode")
 //@Disabled
 public class BasicOpMode_Iterative extends OpMode
 {
